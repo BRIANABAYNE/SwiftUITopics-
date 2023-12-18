97d546ed5b8cf87c20d018a07147e9a31d83df52
Briana Bayne <brianabayne1@gmail.com>
2023-12-18 12:09:48 -0700

    Initial Commit.

---
+++ SwiftUITopics/SwiftUITopics/SwiftUITopicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITopicsApp.swift
+//  SwiftUITopics
+//
+//  Created by Briana Bayne on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITopicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
